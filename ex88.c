6b1f336e80e883aadb72fce0d6878ddae3b2a30b
Helena <helena.petruz@gmail.com>
2026-04-24 06:26:33 -0300

    Update ex88.c

--- ex88.c
+++ ex88.c
@@ -1,35 +1,39 @@
-#include<stdio.h>
-#include<stdbool.h>
-//ARRUMAAAAAAR
-int main(){
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
     int vet[20], busca, novo[19];
     bool existe = false;
+    int j = 0;
 
-    for(int i=0; i<20; i++){
+    for(int i = 0; i < 20; i++) {
         printf("Digite um número: ");
         scanf("%d", &vet[i]);
     }
-    
-    printf("Digite um numero a ser buscado:");
+
+    printf("Digite um numero a ser buscado: ");
     scanf("%d", &busca);
 
-    for(int i=0; i<20; i++){
-        if(vet[i]==busca){
+    for(int i = 0; i < 20; i++) {
+        if(vet[i] == busca) {
             existe = true;
         }
     }
 
-    if(existe==true){
-        for(int i=0; i<20; i++){
-            if(vet[i] != busca){
-                novo[i]=vet[i];
+    if(existe == true) {
+        for(int i = 0; i < 20; i++) {
+            if(vet[i] != busca) {
+                novo[j] = vet[i];
+                j++;
             }
         }
+
+        printf("Vetor novo:\n");
+        for(int i = 0; i < j; i++) {
+            printf("%d\t", novo[i]);
+        }
+    } else {
+        printf("O numero nao existe no vetor.\n");
     }
 
-    printf("Vetor novo: ");
-    for(int i=0; i<20; i++){
-        printf("%d\t", &novo[i]);
-    }
-    
 }
